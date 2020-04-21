699553b84cf5a835c7d5cf69c492acf0ea618aa7
jozua palandi <jozuafp@gmail.com>
2020-04-22 01:30:00 +0700

    Struktur Kontrol while

---
+++ BasicWhile/src/basicwhile/BasicWhile.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicwhile;
+
+/**
+ *
+ * @author JW
+ */
+public class BasicWhile {
+
+    public static void main(String[] args) {
+        int counter = 1;
+        while(counter <= 100)
+        {
+            System.out.println(counter);
+            counter += 2;
+        }
+        System.out.println("Counter = " + counter);
+    }
+    
+}

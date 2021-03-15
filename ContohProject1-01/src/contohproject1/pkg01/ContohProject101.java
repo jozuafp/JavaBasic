1d484f13a57f3f732357918f561f1cdec42effbf
Jozua Palandi <Jozua Palandi@LAPTOP-DHRQMOMT>
2021-03-15 16:46:05 +0700

    Java Basic - Sesi 2 01

---
+++ ContohProject1-01/src/contohproject1/pkg01/ContohProject101.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contohproject1.pkg01;
+
+/**
+ *
+ * @author Jozua Palandi
+ */
+public class ContohProject101 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String NamaDepan, NamaBelakang;
+        int a1=10,a2=20;
+        int hasil;
+        NamaDepan = "Joz";
+        NamaBelakang = "Palandi";
+        System.out.println(NamaDepan+" "+NamaBelakang);
+        System.out.println(a1);
+        System.out.println(a2);
+        hasil=a1*a2;
+        System.out.println(a1+"*"+a2+"="+hasil);
+    }
+    
+}

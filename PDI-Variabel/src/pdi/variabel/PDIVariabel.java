2855206800a25569a61ca653424823b5286262ee
Jozua Palandi <Jozua Palandi@LAPTOP-DHRQMOMT>
2021-03-29 15:30:28 +0700

    PDI - Sesi 2

---
+++ PDI-Variabel/src/pdi/variabel/PDIVariabel.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdi.variabel;
+
+/**
+ *
+ * @author Jozua Palandi
+ */
+public class PDIVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int time = 10, speed = 20;
+        char var = 'h';
+        System.out.println(time);
+        System.out.println("speed = " + speed);
+        System.out.println(var);
+    }
+    
+}

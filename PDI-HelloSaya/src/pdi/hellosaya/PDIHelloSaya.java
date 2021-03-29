bc4a769a7432964f8594774e163baad813db68a8
Jozua Palandi <Jozua Palandi@LAPTOP-DHRQMOMT>
2021-03-29 15:25:26 +0700

    PDI - Sesi 1

---
+++ PDI-HelloSaya/src/pdi/hellosaya/PDIHelloSaya.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdi.hellosaya;
+
+/**
+ *
+ * @author Jozua Palandi
+ */
+public class PDIHelloSaya {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+        System.out.println("Saya");
+        System.out.println("Kamu");
+        System.out.println("");
+        System.out.println("Hello\nSaya\nKamu");
+    }
+    
+}
